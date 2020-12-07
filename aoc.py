d55dd86e3252804a8617d2213c66ce1cd55ed91c
Tobias <devtobb@gmail.com>
2020-12-07 23:14:35 +0100

    add caching to read_input

--- aoc.py
+++ aoc.py
@@ -1,5 +1,31 @@
-# boiler plate
+import pathlib
+import os
+
+CACHE_FOLDER = "input_cache"
+CACHE_FILENAME = "input-{year}-{day:02d}.txt"
+
 def read_input(year, day):
+    # create cache pah if it doesn't exist
+    path = os.path.dirname(__file__)
+    path = os.path.join(path, CACHE_FOLDER)
+    pathlib.Path(path).mkdir(parents=True, exist_ok=True)  
+
+    # check if file exists
+    filename = os.path.join(path, CACHE_FILENAME.format(year=year, day=day))
+    if os.path.isfile(filename):
+        # read input from cache file
+        with open(filename, 'rt') as f:
+            content = f.read()
+    else:
+        # read input from remote, save to file then
+        content = read_remote(year, day)
+        with open(filename, 'wt') as f:
+            f.write(content)
+
+    return content
+
+
+def read_remote(year, day):
     import requests
     import yaml
     
