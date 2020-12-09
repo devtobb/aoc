67377288917c3d075ab7de358b1e9b022c9028ce
tobbe <devtobb@gmail.com>
2020-12-09 12:18:26 +0100

    use operators instead of lambdas

--- 2020-06.py
+++ 2020-06.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from functools import reduce
+from operator import and_, or_
 
 from aoc import read_input
 
@@ -8,10 +9,10 @@ def group_reduce(groups, func):
     return sum(len(reduce(func, group)) for group in groups)
 
 def puzzle1(groups):
-    return group_reduce(groups, lambda a,b:a|b)
+    return group_reduce(groups, or_)
 
 def puzzle2(groups):
-    return group_reduce(groups, lambda a,b:a&b)
+    return group_reduce(groups, and_)
 
 raw = read_input(2020, 6).split('\n\n')
 groups = [list(map(set, group.split())) for group in raw]
