da60f372760b5b43a614f1eea3062dd03fc8c9cd
tobbe <devtobb@gmail.com>
2020-12-15 13:38:54 +0100

    part ugly but working

--- 2020-14.py
+++ 2020-14.py
@@ -18,7 +18,22 @@ def puzzle1(instr):
     return sum(mem.values())
 
 def puzzle2():
-    pass
+    masks  = []
+    mem = {}
+    for i in instr:
+        if i['mask']:
+            n = i['mask'].count('X')
+            mask_1 = int(i['mask'].translate({88:49}), 2)
+            repl = [ list(("{:0"+str(n)+"b}").format(m)) for m in range(2**n)] 
+            mask_template = i['mask'].translate({88:'{}', 48:49})
+            masks = [ int(mask_template.format(*r), 2) for r in repl]
+        else:
+            for mask in masks:
+                addr = (int(i['addr']) | mask_1) & mask
+                mem[addr] = int(i['value'])
+
+    return sum(mem.values())
+
 
 r = r"(mask = (?P<mask>[10X]+))|(mem\[(?P<addr>\d+)\] = (?P<value>\d+))"
 raw = read_input(2020, 14).split('\n')[:-1]
