a0e38d3e99290df395421c509048399d32880179
tobbe <devtobb@gmail.com>
2020-12-25 17:02:24 +0100

    clean up part 1

--- 2020-23.py
+++ 2020-23.py
@@ -2,26 +2,19 @@
 
 from aoc import read_input
 
-class blist(list):
-    def __rshift__(self, f):
-        if type(f)==tuple: f, *args = f
-        else: args = ()
-        return blist(f(x, *args) for x in self)
-
 def move(cups, current):
-    idx_cur, *_ = [n for n, cup in enumerate(cups) if cup==current]
+    idx_cur = cups.index(current)
     idx_pu_start = (idx_cur + 1) % len(cups)
+    
     if len(cups) - idx_pu_start < 3:
         idx_pu_end = (idx_pu_start + 3) % len(cups)
-    else:
-        idx_pu_end = idx_pu_start + 3
-
-    if idx_pu_start < idx_pu_end:
-        pu = cups[idx_pu_start:idx_pu_end]
-        rest = cups[:idx_pu_start] + cups[idx_pu_end:]
-    else:
         pu = cups[idx_pu_start:] + cups[:idx_pu_end]
         rest = cups[idx_pu_end:idx_pu_start]
+    else:
+        idx_pu_end = idx_pu_start + 3
+        pu = cups[idx_pu_start:idx_pu_end]
+        rest = cups[:idx_pu_start] + cups[idx_pu_end:]
+
     
     dest = current - 1
     while not dest in rest:
@@ -29,37 +22,29 @@ def move(cups, current):
         if dest < min(rest):
             dest = max(rest)
 
-    idx_dest, *_ = [n for n, cup in enumerate(rest) if cup==dest]
-
+    idx_dest = rest.index(dest)
     idx_dest = (idx_dest + 1) % len(cups)
-
-    # print(f"cups: {cups} current: {current}")
-    # print(f"pu: {pu} [{idx_pu_start}, {idx_pu_end}]")
-    # print(f"dest: {dest} ({idx_dest})")
-
-    ret = rest[:idx_dest] + pu + rest[idx_dest:]
-    # print(f"ret: {ret}")
-    # print()
-    idx_cur_new, *_ = [n for n, cup in enumerate(ret) if cup==current]
-    cur_new = ret[(idx_cur_new + 1)%len(cups)]
+    cups = rest[:idx_dest] + pu + rest[idx_dest:]
+    idx_cur_new = cups.index(current)
+    cur_new = cups[(idx_cur_new + 1)%len(cups)]
     
-    return ret, cur_new
+    return cups, cur_new
 
 def puzzle1(cups):
     current, *_ = cups
     for _ in range(100):
         cups, current = move(cups, current)
 
-    idx_one, *_ = [n for n, cup in enumerate(cups) if cup==1]
+    idx_one = cups.index(1)
     
-    return "".join(blist((cups[idx_one+1:] + cups[:idx_one])) >> str)
+    return "".join(map(str, cups[idx_one+1:] + cups[:idx_one]))
 
 def puzzle2():
     pass
 
 raw = read_input(2020, 23)
 # raw = '389125467\n'
-cups = blist(raw[:-1]) >> int
+cups = list(map(int, list(raw[:-1])))
 
 
 print(f"Puzzle 1: {puzzle1(cups)}")
