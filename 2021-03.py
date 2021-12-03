e3a3d73c4eb951536004a8562c6c7aa61057bd61
tobbe <devtobb@gmail.com>
2021-12-03 21:13:29 +0100

    day 3 cleaner

--- 2021-03.py
+++ 2021-03.py
@@ -1,50 +1,39 @@
 #!/usr/bin/env python3
-import numpy as np
+from copy import deepcopy
+from math import prod
+from operator import ge, lt
 
 from aoc import read_input
 
-def freqs(raw):
-    ret = np.zeros((12, 2))
-    for n in range(12):
-        _, freq = np.unique(raw[:, n], return_counts=True)
-        ret[n, :] = freq
-    return ret
+def colsum(raw, col):
+    return sum((l[col] for l in raw))
 
-def most_common(raw):
-    freq = freqs(raw)
-    freq[:,1] = freq[:,1] + (freq[:,0]==freq[:,1])
-    return np.argmax(freq, axis=1)
-
-def least_common(raw):
-    freq = freqs(raw)
-    freq[:,0] = freq[:,0] - (freq[:,0]==freq[:,1])
-    return np.argmin(freq, axis=1)
-
-def arr2bin(arr):
-    return int("".join(map(str, arr)), 2)
+def bin_to_int(l):
+    return int("".join(map(str, map(int, l))), 2)
 
 def puzzle1(raw):
-    return arr2bin(most_common(raw)) * arr2bin(least_common(raw))
+    gamma = [colsum(raw, col)>len(raw)/2 for col in range(12)]
+    epsilon = [colsum(raw, col)<len(raw)/2 for col in range(12)]
+    
+    return bin_to_int(gamma) * bin_to_int(epsilon)
 
 def puzzle2(raw):
-    ox = np.copy(raw)
-    n = 0
-    while len(ox) > 1:
-        mc = most_common(ox)
-        ox = ox[ox[:, n]==mc[n], :]
-        n += 1
+    result = []
+    for op in ge, lt:
+        lst = deepcopy(raw)
+        col = 0
+        while len(lst) > 1:
+            lst = list(filter(lambda l: l[col]==op(colsum(lst, col), len(lst)/2), lst))
+            col += 1
 
-    co = np.copy(raw)
-    n = 0
-    while len(co) > 1:
-        lc = least_common(co)
-        co = co[co[:, n]==lc[n], :]
-        n += 1
+        remaining, *_ =  lst
+        result.append(bin_to_int(remaining))
+    
+    return prod(result)
 
-    return arr2bin(ox[0]) * arr2bin(co[0])
 
 raw = read_input(2021, 3)
-raw = np.array([[int(e) for e in l] for l in raw.split()])
+raw = [[int(e) for e in l] for l in raw.split()]
 
 print(f"Puzzle 1: {puzzle1(raw)}")
 print(f"Puzzle 2: {puzzle2(raw)}")
