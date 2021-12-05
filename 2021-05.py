f9b0be79a2d7017ecbf4598cfac60771043eb126
Tobias <devtobb@gmail.com>
2021-12-05 17:07:16 +0100

    use defaultdict to speed up proceessing

--- 2021-05.py
+++ 2021-05.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
+from collections import defaultdict
 from math import copysign
 from itertools import repeat
 
 from aoc import read_input
-
 def lrng(a, b):
     if a==b:
         return repeat(a)
@@ -12,10 +12,10 @@ def lrng(a, b):
         return range(a, b+sgn, sgn)
 
 def dangerous(lines):
-    field = dict()
+    field = defaultdict(int)
     for (x1, y1), (x2, y2) in lines:
         for x, y in zip(lrng(x1, x2), lrng(y1, y2)):
-            field[(x,y)] = (field.get((x, y)) or 0) + 1 
+            field[(x,y)] += 1
 
     return sum(( 1 for v in field.values() if v > 1))
 
@@ -30,7 +30,9 @@ def puzzle2(lines):
     return dangerous(lines)
 
 raw = read_input(2021, 5)
-lines = [[list(map(int,p.split(","))) for p in l.split(" -> ")] for l in raw[:-1].split("\n")]
+lines = [[list(map(int,p.split(","))) 
+            for p in l.split(" -> ")] 
+            for l in raw[:-1].split("\n")]
 
 print(f"Puzzle 1: {puzzle1(lines)}")
 print(f"Puzzle 2: {puzzle2(lines)}")
