778173e17c00993c3ff70204350491dda8098eba
Tobias <devtobb@gmail.com>
2021-12-06 20:12:40 +0100

    calculate n_fish only one time

--- 2021-06.py
+++ 2021-06.py
@@ -2,8 +2,7 @@
 
 from aoc import read_input
 
-def run_days(fish, days):
-    n_fish = [fish.count(d) for d in range(9)]
+def run_days(n_fish, days):
     zero = 0
     for d in range(days):
         zero = (zero + 1) % 9
@@ -11,14 +10,15 @@ def run_days(fish, days):
 
     return sum(n_fish)
 
-def puzzle1(fish):
-    return run_days(fish, 80)
+def puzzle1(n_fish):
+    return run_days(n_fish, 80)
 
-def puzzle2(fish):
-    return run_days(fish, 256)
+def puzzle2(n_fish):
+    return run_days(n_fish, 256)
 
 raw = read_input(2021, 6)
 fish = list(map(int, raw.split(','))) 
+n_fish = [fish.count(d) for d in range(9)]
 
-print(f"Puzzle 1: {puzzle1(fish)}")
-print(f"Puzzle 2: {puzzle2(fish)}")
+print(f"Puzzle 1: {puzzle1(n_fish.copy())}")
+print(f"Puzzle 2: {puzzle2(n_fish.copy())}")
