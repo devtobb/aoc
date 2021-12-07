a43583d8da92425bd5b65f77161c244e3870d0c2
Tobias <devtobb@gmail.com>
2021-12-07 16:28:37 +0100

    day 7

---
+++ 2021-07.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from statistics import median
+
+from aoc import read_input
+
+def puzzle1(crabs):
+    m = median(crabs)
+    return int(sum((abs(m-c) for c in crabs)))
+
+def puzzle2(crabs):
+    opt = sum(crabs)//len(crabs)
+    return sum(((abs(c-opt)*(abs(c-opt)+1)) // 2) for c in crabs)
+
+raw = read_input(2021, 7)
+crabs = list(map(int, raw.split(",")))
+
+print(f"Puzzle 1: {puzzle1(crabs)}")
+print(f"Puzzle 2: {puzzle2(crabs)}")
