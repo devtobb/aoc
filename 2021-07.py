63eb31b763576f3c823098a55af259fd198d346a
Tobias <devtobb@gmail.com>
2021-12-07 18:24:28 +0100

    fix edge cases

--- 2021-07.py
+++ 2021-07.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
-from statistics import median
+from statistics import median, mean
 
+from math import floor, ceil
 from aoc import read_input
 
+def solve(crabs, cost, center):
+    m = center(crabs)
+    opt =  [floor(m), ceil(m)]
+    return int(min((sum((cost(c, o) for c in crabs)) for o in opt)))
+
 def puzzle1(crabs):
-    opt = median(crabs)
-    return int(sum((abs(opt-c) for c in crabs)))
+    cost = lambda crab, pos: abs(crab - pos)
+    return solve(crabs, cost, median)
 
 def puzzle2(crabs):
-    opt = sum(crabs)//len(crabs)
-    return sum(((abs(c-opt)*(abs(c-opt)+1)) // 2) for c in crabs)
+    cost = lambda crab, pos: 0.5*abs(crab-pos)*(abs(crab-pos)+1)
+    return solve(crabs, cost, mean)
 
 raw = read_input(2021, 7)
 crabs = list(map(int, raw.split(",")))
