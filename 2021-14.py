035fac8b26cce0b5a5141679f256bc9626015485
tobbe <devtobb@gmail.com>
2021-12-14 20:18:08 +0100

    use gmp for arbitray large ints

--- 2021-14.py
+++ 2021-14.py
@@ -4,7 +4,9 @@ from collections import defaultdict, Counter
 from math import ceil
 import re
 
+from gmpy2 import mpz
 import numpy as np
+
 
 from aoc import read_input
 
@@ -12,22 +14,22 @@ def synthesize(start, subs, elements, rounds):
     pairs = [a+b for a in elements for b in elements]
     n_elements = len(elements)
 
-    m = np.zeros((n_elements**2,)*2, dtype=object)
+    m = np.zeros((n_elements**2,)*2, dtype=mpz)
 
     for (e1, e2), e3 in subs.items():
         m[pairs.index(e1+e2)][pairs.index(e1+e3)] += 1
         m[pairs.index(e1+e2)][pairs.index(e3+e2)] += 1
 
-    v = np.zeros(n_elements**2, dtype=object)
+    v = np.zeros(n_elements**2, dtype=mpz)
     for e1, e2 in zip(start, start[1:]):
         v[pairs.index(e1+e2)] += 1
 
-    r10 = v @ np.linalg.matrix_power(m, rounds)
+    result = v @ np.linalg.matrix_power(m, rounds)
 
-    count = Counter()
+    count = defaultdict(mpz)
     for e1, e2 in pairs:
-        count[e1] += r10[pairs.index(e1+e2)]
-        count[e2] += r10[pairs.index(e1+e2)]
+        count[e1] += result[pairs.index(e1+e2)]
+        count[e2] += result[pairs.index(e1+e2)]
 
     least, *_, most = sorted(map(lambda c: ceil(c/2), count.values()))
 
@@ -38,5 +40,5 @@ start, subs = raw.split('\n\n')
 subs = dict(re.findall("(\w+) -> (\w)+", subs))
 elements = list(set(re.findall("\w", raw)))
 
-print(f"\033[97m★\033[00m {synthesize(start, subs, elements, 10)}")
-print(f"\033[93m★\033[00m {synthesize(start, subs, elements, 40)}")
+print(f"\033[97m★\033[00m {synthesize(start, subs, elements, 65_536)}")
+# print(f"\033[93m★\033[00m {synthesize(start, subs, elements, 40)}")
