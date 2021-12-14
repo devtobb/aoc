81ed0024e4bafa0b82bb4e75390981eaa54b89a1
tobbe <devtobb@gmail.com>
2021-12-14 21:11:21 +0100

    custom fast matrix exponentiation

--- 2021-14.py
+++ 2021-14.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from collections import defaultdict, Counter
+from collections import defaultdict
 from math import ceil
 import re
 
@@ -9,6 +9,16 @@ import numpy as np
 
 
 from aoc import read_input
+
+def fast_exp(m, p):
+    if p==1:
+        return m
+    else:
+        ret = fast_exp(m, p//2)
+        ret = ret @ ret
+        if p%2 > 0:
+            ret = ret @ fast_exp(m, p%2)
+        return ret
 
 def synthesize(start, subs, elements, rounds):
     pairs = [a+b for a in elements for b in elements]
@@ -24,7 +34,7 @@ def synthesize(start, subs, elements, rounds):
     for e1, e2 in zip(start, start[1:]):
         v[pairs.index(e1+e2)] += 1
 
-    result = v @ np.linalg.matrix_power(m, rounds)
+    result = v @ fast_exp(m, rounds)
 
     count = defaultdict(mpz)
     for e1, e2 in pairs:
@@ -33,12 +43,12 @@ def synthesize(start, subs, elements, rounds):
 
     least, *_, most = sorted(map(lambda c: ceil(c/2), count.values()))
 
-    return most - least
+    return int(most - least)
 
 raw = read_input(2021, 14)
 start, subs = raw.split('\n\n')
 subs = dict(re.findall("(\w+) -> (\w)+", subs))
 elements = list(set(re.findall("\w", raw)))
 
-print(f"\033[97m★\033[00m {synthesize(start, subs, elements, 65_536)}")
-# print(f"\033[93m★\033[00m {synthesize(start, subs, elements, 40)}")
+print(f"\033[97m★\033[00m {synthesize(start, subs, elements, 10)}")
+print(f"\033[93m★\033[00m {synthesize(start, subs, elements, 40)}")
