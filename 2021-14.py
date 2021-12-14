b6731b6a02c84ec5d3f577b9690ef5622f2baedd
tobbe <devtobb@gmail.com>
2021-12-14 13:14:23 +0100

    some cleanup

--- 2021-14.py
+++ 2021-14.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-from copy import deepcopy
-from collections import Counter, defaultdict
+from copy import copy
+from collections import defaultdict
 import re
 
 from aoc import read_input
@@ -11,21 +11,22 @@ def synthesize(start, subs, rounds):
     counts = defaultdict(int)
     for e1, e2 in zip(start[:-1], start[1:]):
         counts[e1+e2] += 1
-        
+    
     for _ in range(rounds):
-        new_counts = deepcopy(counts)
+        counts_new = copy(counts)
         for e1, e2 in subs:
-            new_counts[e1+subs[e1+e2]] += counts[e1+e2]
-            new_counts[subs[e1+e2]+e2] += counts[e1+e2]
-            new_counts[e1+e2] -= counts[e1+e2]
-        counts = new_counts
+            e3 = subs[e1 + e2]
+            counts_new[e1 + e3] += counts[e1 + e2]
+            counts_new[e3 + e2] += counts[e1 + e2]
+            counts_new[e1 + e2] -= counts[e1 + e2]
+        counts = counts_new
     
-    final = defaultdict(int)
+    count = defaultdict(int)
     for e1, e2 in counts:
-        final[e1] += counts[e1+e2]
-        final[e2] += counts[e1+e2]
+        count[e1] += counts[e1 + e2]
+        count[e2] += counts[e1 + e2]
     
-    count = sorted([final[e]//2 for e in final if e.isalpha()])
+    count = sorted([count[e]//2 for e in count if e.isalpha()])
     least, *_ = count
     *_, most = count
     return most - least
