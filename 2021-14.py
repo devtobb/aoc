f8fe9c7654ba116d559b685aed6cea18ae52d6cc
tobbe <devtobb@gmail.com>
2021-12-14 13:57:46 +0100

    shave one line

--- 2021-14.py
+++ 2021-14.py
@@ -9,12 +9,11 @@ def synthesize(start, subs, rounds):
     start = "#" + start + "#"
     counts = defaultdict(int)
     for e1, e2 in zip(start, start[1:]):
-        counts[e1+e2] += 1
+        counts[e1 + e2] += 1
     
     for _ in range(rounds):
         counts_new = defaultdict(int)
-        for e1, e2 in subs:
-            e3 = subs[e1 + e2]
+        for (e1, e2), e3 in subs.items():
             counts_new[e1 + e3] += counts[e1 + e2]
             counts_new[e3 + e2] += counts[e1 + e2]
         counts = counts_new
@@ -29,7 +28,7 @@ def synthesize(start, subs, rounds):
 
 raw = read_input(2021, 14)
 start, subs = raw.split('\n\n')
-subs = dict(re.findall("(\w+) -> (\w)+\n", subs))
+subs = dict(re.findall("(\w+) -> (\w)+", subs))
 
 print(f"\033[97m★\033[00m {synthesize(start, subs, 10)}")
 print(f"\033[93m★\033[00m {synthesize(start, subs, 40)}")
