731dd6c8c50ddef67acb64b59b4b3fdf84031b3f
tobbe <devtobb@gmail.com>
2023-12-01 12:00:44 +0100

    empty template for an old day

---
+++ 2021-18.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from aoc import read_input
+
+def puzzle1():
+    pass
+
+def puzzle2():
+    pass
+
+raw = read_input(2021, 18)
+
+print(f"\033[97m★\033[00m {puzzle1()}")
+print(f"\033[93m★\033[00m {puzzle2()}")
