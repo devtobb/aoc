a593ad72a36b2c5b1f21a4172cd143173aa85acd
Tobias <devtobb@gmail.com>
2023-12-07 17:17:33 +0100

    replace object with named tuple

--- 2023/2023-07.py
+++ 2023/2023-07.py
@@ -1,33 +1,29 @@
 #!/usr/bin/env python3
 
-from collections import Counter
+from collections import Counter, namedtuple
 
 from tools import read_input
 
 ORDER = {False :  "23456789TJQKA", True :  "J23456789TQKA"}
+Hand = namedtuple('Hand', ['cards', 'bid'])
 
-class Hand(object):
-    def __init__(self, line):
-        self.cards, bid = line.split()
-        self.bid = int(bid)
-
-    def order(self, joker=False):
-        cards = self.cards.replace('J', '') if joker else self.cards
-        counter = Counter(cards)
-        frequencies = sorted(counter.values(), reverse=True)
-        most_frequent, second_most_frequent, *_ = frequencies + [0, 0]
-        most_frequent += joker * self.cards.count('J')
-        order = (most_frequent << 1) + second_most_frequent
-        for card in self.cards:
-            order = (order << 4) + ORDER[joker].index(card)
-        return order
+def order(hand, joker=False):
+    cards = hand.cards.replace('J', '') if joker else hand.cards
+    counter = Counter(cards)
+    frequencies = sorted(counter.values(), reverse=True)
+    most_frequent, second_most_frequent, *_ = frequencies + [0, 0]
+    most_frequent += joker * hand.cards.count('J')
+    order = (most_frequent << 1) + second_most_frequent
+    for card in hand.cards:
+        order = (order << 4) + ORDER[joker].index(card)
+    return order
         
 def score(hands, joker):
-    hands = sorted(hands, key=lambda h: h.order(joker))
+    hands = sorted(hands, key=lambda hand: order(hand, joker))
     return sum(n*hand.bid for n, hand in enumerate(hands, 1))
 
-raw = read_input(2023, 7, 'bb').splitlines()
-hands = tuple(map(Hand, raw))
+raw = read_input(2023, 7, 'bb').split()
+hands = [Hand(cards, int(bid)) for cards, bid in zip(raw[::2], raw[1::2])]
 
 print(f"\033[97m★\033[00m {score(hands, False)}")
 print(f"\033[93m★\033[00m {score(hands, True)}")
