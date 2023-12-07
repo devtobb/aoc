539e6480224f22f26382fa0598e4dd065aa54fb0
Tobias <devtobb@gmail.com>
2023-12-07 20:22:42 +0100

    even more unreadable :)

--- 2023/2023-07-golf.py
+++ 2023/2023-07-golf.py
@@ -2,12 +2,12 @@ from collections import Counter
 from functools import reduce
 from tools import read_input
 
-def order(c, mask):
-    c = c.translate(str.maketrans('TJQKA', mask))
-    f, s, *_ = sorted(Counter(c.replace('0', '') if mask==':0<=>' else c).values(), reverse=True)+[0,0]
-    return reduce(lambda o, ch: (o<<4) + ord(ch), c, ((f+(mask==':0<=>')*c.count('0'))<<1)+s)
+def order(c, m):
+    c = c.translate(str.maketrans('TJQKA', m))
+    f = [0,0]+sorted(Counter(c.replace('0','') if m==':0<=>' else c).values())
+    return (((f[-1]+(mask==':0<=>')*c.count('0'))<<1)+f[-2]), c
         
-raw = read_input(2023, 7, 'bb').split()
-hands = [(cards, int(bid)) for cards, bid in zip(raw[::2], raw[1::2])]
-for mask, color in ((':;<=>', 97), (':0<=>', 93)):
-    print(f"\033[{color}m★\033[00m {sum(n*h[1] for n, h in enumerate(sorted(hands, key=lambda h: order(h[0], mask)), 1))}")
+raw, e, s = read_input(2023, 7, 'bb').split(), enumerate, sorted
+hd = [(cards, int(bid)) for cards, bid in zip(raw[::2], raw[1::2])]
+for mask in ':;<=>', ':0<=>':
+    print(sum(n*h[1] for n, h in e(s(hd, key=lambda h: order(h[0], mask)), 1)))
