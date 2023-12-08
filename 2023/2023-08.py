24048a646b9d5e31aea675f05ef88cd8905bf51a
tobbe <devtobb@gmail.com>
2023-12-08 10:20:20 +0100

    don't explicitly pass nodes and instructions

--- 2023/2023-08.py
+++ 2023/2023-08.py
@@ -5,21 +5,21 @@ from math import lcm
 
 from tools import read_input
 
-def n_steps(start, nodes, instr, stop):
+def n_steps(start, stop):
     for n, i in enumerate(cycle(instr), 1):
         start = nodes[start][i]
         if stop(start):
             return n
 
-def puzzle1(nodes, instr):
-    return n_steps('AAA', nodes, instr, lambda p: p=='ZZZ')
+def puzzle1():
+    return n_steps('AAA', lambda p: p=='ZZZ')
 
-def puzzle2(nodes, instr):
-    return lcm(*(n_steps(start, nodes, instr, lambda p: p.endswith('Z')) for start in nodes.keys() if start.endswith('A')))
+def puzzle2():
+    return lcm(*(n_steps(start, lambda p: p.endswith('Z')) for start in nodes.keys() if start.endswith('A')))
 
 raw = read_input(2023, 8)
 instr, nodes = raw.split('\n\n')
 nodes = {l[0:3]:dict(L=l[7:10], R=l[12:15]) for l in nodes.splitlines()}
 
-print(f"\033[97m★\033[00m {puzzle1(nodes, instr)}")
-print(f"\033[93m★\033[00m {puzzle2(nodes, instr)}")
+print(f"\033[97m★\033[00m {puzzle1()}")
+print(f"\033[93m★\033[00m {puzzle2()}")
