22009859f89e1086130ddd11e2869fc6ed795af5
tobbe <devtobb@gmail.com>
2023-12-08 10:15:14 +0100

    more compact

--- 2023/2023-08.py
+++ 2023/2023-08.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from itertools import cycle
-from functools import reduce
 from math import lcm
 
 from tools import read_input
@@ -18,12 +17,9 @@ def puzzle1(nodes, instr):
 def puzzle2(nodes, instr):
     return lcm(*(n_steps(start, nodes, instr, lambda p: p.endswith('Z')) for start in nodes.keys() if start.endswith('A')))
 
-
 raw = read_input(2023, 8)
 instr, nodes = raw.split('\n\n')
-instr = list(instr)
-nodes = nodes.splitlines()
-nodes = {l[0:3]:dict(L=l[7:10], R=l[12:15]) for l in nodes}
+nodes = {l[0:3]:dict(L=l[7:10], R=l[12:15]) for l in nodes.splitlines()}
 
 print(f"\033[97m★\033[00m {puzzle1(nodes, instr)}")
 print(f"\033[93m★\033[00m {puzzle2(nodes, instr)}")
