ddf72a1bc769a3ee35409e0ba6a1a6cec02acb2f
tobbe <devtobb@gmail.com>
2023-12-09 12:55:07 +0100

    calculate derivatives only one time for both parts

--- 2023/2023-09.py
+++ 2023/2023-09.py
@@ -2,21 +2,23 @@
 
 from tools import *
 
-def interp(hist, sgn, idx):
+def interp(hist):
     if not any(hist):
-        return 0
+        return (0, 0)
     (*head, _), (_, *tail) = hist, hist
     deriv = list( b-a for a, b in zip(head, tail))
-    return sgn*interp(deriv, sgn, idx) + hist[idx]
+    front, back = interp(deriv)
+    return hist[0] - front, hist[-1] + back
 
-def puzzle1(hists):
-    return sum(hists >> (lambda h: interp(h, 1, -1)))
+def puzzle1(values):
+    return sum(back for _, back in values)
 
-def puzzle2(hists):
-    return sum(hists >> (lambda h: interp(h, -1, 0)))
+def puzzle2(values):
+    return sum(front for front, _ in values)
 
-raw = read_input(2023, 9)
+raw = read_input(2023, 9, 'bb')
 hists = blist(map(ints, raw.splitlines()))
+values = hists >> interp
 
-print(f"\033[97m★\033[00m {puzzle1(hists)}")
-print(f"\033[93m★\033[00m {puzzle2(hists)}")
+print(f"\033[97m★\033[00m {puzzle1(values)}")
+print(f"\033[93m★\033[00m {puzzle2(values)}")
