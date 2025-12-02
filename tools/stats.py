888a81cd795e412b2467b1b350513ecc576dcca7
Tobias <devtobb@gmail.com>
2025-12-02 17:58:46 +0100

    update stats for 2025

--- tools/stats.py
+++ tools/stats.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from math import floor, ceil
 import re
 from pprint import pprint
@@ -7,13 +9,27 @@ import matplotlib.pyplot as plt
 import requests
 import yaml
 
+def row_to_user(row):
+    try:
+        position = int(re.sub(r"\D", "", row.find('span', {'class': 'privboard-position'}).decode_contents()))
+        score = int(re.sub(r"\D", "", row.find('span', {'class': 'privboard-position'}).find_next_sibling(string=True)))
+        name = row.find('span', {'class': 'privboard-name'}).decode_contents().strip()
+
+        return dict(
+            position=position, 
+            score=score,
+            name=name
+        )
+    except Exception as e:
+        return None
+    
 with open("cookies.yaml", "rt") as f:
         cookies = yaml.load(f, yaml.FullLoader)
 
 
 boards = [
-        dict(name='pro', url="https://adventofcode.com/2020/leaderboard/private/view/198336"),
-        dict(name='noob', url="https://adventofcode.com/2020/leaderboard/private/view/993406")
+        dict(name='anon', url="https://adventofcode.com/2025/leaderboard/private/view/383378"),
+        dict(name='namefags', url="https://adventofcode.com/2025/leaderboard/private/view/993406")
         ]
 
 users = dict()
@@ -24,12 +40,12 @@ for board in boards:
         cookies=cookies
     )
 
-    RE_BOARD_ROW = r"^\s*(?P<place>\d+)\)\s+(?P<score>\d+)\s+\*+\s+(?P<name>.+)$"
-
     soup = BeautifulSoup(r.text, 'html.parser')
-    users[board['name']] = [re.match(RE_BOARD_ROW, div.text).groupdict() for div in soup.find_all('div', {'class':'privboard-row'})[1:]]
+    rows = [name.parent for name in soup.find_all('span', {'class':'privboard-name'})]
+    users[board['name']] = list(filter(None, map(row_to_user, rows)))
     me, *_ = [user for user in users[board['name']] if user['name'] == '(anonymous user #1173836)']
-    pprint("{} : {}".format(board['name'], me))
+    print(f"{'#'*10} {board['name']} {'#'*10}")
+    pprint(me)
 
 plot_width = ceil(len(boards)**0.5)
 plot_height = floor(len(boards)**0.5)
@@ -41,9 +57,6 @@ for n, board in enumerate(boards):
     ax[n].set_title(board['name'])
     mepos, *_ = [i for i, user in enumerate(users[board['name']]) if user['name'] == '(anonymous user #1173836)']
     colors = ['gray']*mepos + ['red'] + ['gray']*((len(users[board['name']]) - mepos)-1)
-    # print(colors)
-    # print(len(colors))
-    # print(len(users[board['name']]))
     ax[n].bar(range(1, len(users[board['name']])+1), [int(user['score']) for user in users[board['name']]], color=colors)
 
 plt.show()
